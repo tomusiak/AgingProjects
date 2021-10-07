c41483a884a0334e936d7759819ec559318fb8f3
tomusiak <atomusiak6@gmail.com>
2021-10-06 17:23:12 -0700

    Updated graphs

--- PCA_umap.R
+++ PCA_umap.R
@@ -5,6 +5,12 @@ library(factoextra)
 library(dplyr)
 library(umap)
 library(janitor)
+library(corrplot)
+library(reshape)
+library(Hmisc)
+library(tidyverse)
+
+
 setwd("/home/atom/Desktop/AgingProjects/")
 data <- read_csv("20donors.csv", col_names=FALSE)
 data <- data.frame(data)
@@ -16,27 +22,21 @@ flipped_data <- flipped_data[-1,]
 flipped_data[ , ] <- apply(flipped_data[ , ], 2,            # Specify own function within apply
                     function(x) as.numeric(as.character(x)))
 rownames(flipped_data) <- c(1:30)
-flipped_data <- clean_names(flipped_data)
 colnames(flipped_data)
-ggplot(flipped_data, aes(age, live_t_cells_cd8_ro_neg_terminal_effectors_fdg_pos)) +
+ggplot(flipped_data, aes(age, fdg_high_temra_cd4s)) +
   geom_point() +
   geom_smooth(method=lm)
 
-ggscatter(flipped_data, x = "age", y = "live_t_cells_cd8_ro_neg_naive_and_early_memory_early_memory_fdg_neg", 
+ggscatter(flipped_data, x = "age", y = "FDG_Pos_EM_CD4s", 
           add = "reg.line", conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "pearson",
-          xlab = "Age", ylab = "% SA-b-Gal Positive Effector Memory CD8s ")
+          xlab = "Age", ylab = "% FDG High Central Memory CD8s ")
 
 aged_data <- subset(flipped_data, select = -c(age) )
 aged_data <- data.frame(scale(aged_data))
 pca <-prcomp(aged_data)
 fviz_eig(pca)
 pca_plus_age <- cbind(pca,aged_data$age)
-fviz_pca_ind(pca,
-             col.ind = "cos2", # Color by the quality of representation
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE     # Avoid text overlapping
-)
 fviz_pca_var(pca,
              col.var = "contrib", # Color by contributions to the PC
              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
@@ -44,41 +44,44 @@ fviz_pca_var(pca,
 )
 factors <- data.frame(pca$x)
 factors$age <- flipped_data$age
-ggscatter(factors, x = "age", y = "PC1", 
+ggscatter(factors, x = "age", y = "PC1",
           add = "reg.line", conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "pearson",
-          xlab = "Age", ylab = "PC1")
-
-fdg_data <- select(aged_data,contains("FDG"))
-fdg_pca <-prcomp(fdg_data)
-fviz_eig(fdg_pca)
-fviz_pca_ind(fdg_pca,
-             col.ind = "cos2", # Color by the quality of representation
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE     # Avoid text overlapping
-)
-fviz_pca_var(fdg_pca,
-             col.var = "contrib", # Color by contributions to the PC
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE     # Avoid text overlapping
-)
-factors <- data.frame(fdg_pca$x)
-factors$age <- flipped_data$age
-factors$nemfdgpos <- flipped_data$live_t_cells_cd8_ro_neg_naive_and_early_memory_early_memory_fdg_pos
-ggscatter(factors, x = "PC1", y = "nemfdgpos", 
-          add = "reg.line", conf.int = TRUE, 
-          cor.coef = TRUE, cor.method = "pearson",
-          xlab = "PC1", ylab = "Age")
-ggscatter(factors, x = "PC1", y = "PC2", color="nemfdgneg", 
-          add = "reg.line", conf.int = TRUE, 
-          cor.coef = TRUE, cor.method = "pearson",
-          xlab = "PC2", ylab = "Age")
+          xlab = "age", ylab = "PC1")
 
 aged_umap <- umap(aged_data,labels = factors$age)
 df <- data.frame(PC1 = aged_umap$layout[,1],
                  PC2 = aged_umap$layout[,2],
                  Age = factors$age)
-ggscatter(df, x = "PC2", y = "Age", 
+ggscatter(df, x= "PC1", y="PC2", color="Age")
+ggscatter(df, x = "Age", y = "PC1", 
           add = "reg.line", conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "pearson",
-          xlab = "PC2", ylab = "Age")
+          xlab = "Age", ylab = "PC2")
+
+correlation <- cor(flipped_data)
+correlation_test <- rcorr(as.matrix(flipped_data))
+heatmap(correlation)
+age_correlation <- data.frame(data.frame(correlation)[,1])
+rownames(age_correlation) <- rownames(correlation)
+age_correlation_test <- data.frame(correlation_test[3])
+age_correlation_test <- data.frame(age_correlation_test[,1])
+rownames(age_correlation_test) <- rownames(correlation)                                   
+age_correlation <- tail(age_correlation,20)
+age_correlation$Marker <- rownames(age_correlation)
+age_correlation_test$Marker <- rownames(age_correlation_test)
+age_correlation_test <- tail(age_correlation_test,20)
+age_correlation$Significance <- age_correlation_test[,1]
+colnames(age_correlation) <- c("Correlation", "Marker", "Significance")
+age_correlation<- age_correlation %>% arrange(Correlation)
+age_correlation$Significance <- round(age_correlation$Significance,3)
+
+ggplot(age_correlation, aes(x = Correlation, y = reorder(Marker, -Correlation), fill=Significance)) + 
+  geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  geom_text(label=age_correlation$Significance,hjust=-.1) +
+  theme(
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    panel.background = element_rect(fill = "transparent",colour = NA),
+    plot.background = element_rect(fill = "transparent",colour = NA)
+  ) + ylab("Marker") + xlab("Correlation")
