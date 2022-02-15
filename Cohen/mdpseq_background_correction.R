101aad78c58821e7156371b0b7b1cad2023ad898
tomusiak <atomusiak6@gmail.com>
2022-02-15 12:30:18 -0800

    bunch of bugfixes, lots of comments

--- Cohen/mdpseq_background_correction.R
+++ Cohen/mdpseq_background_correction.R
@@ -1,72 +1,57 @@
-#load packages
+#Alan Tomusiak
+
+#Contains functions useful for MDPSeq background correction.
+# Functions by processing changes relative to the mean in the mitochondrial genes (hereafter,
+# mitogenes), parsing an "encompass table" that maps MDPs to mitogenes and their proportion of
+# overlap, and then subtracting the signal in the mitogenes.
+# Can be useful in datasets where large changes in mitogene expression can skew MDPSeqs, and
+# intuitively the code has seemed to perform well in practice.
+
+#A few caveats:
+# 1) Does not incorporate strand-specific information. In fact, it is likely that this will
+# provide false signal with strand-specific data. If desired, the code could actually be fairly easily
+# altered to use the strand-specific data effectively.
+#
+# 2) If an MDP overlaps two mitogenes, it will only background correct for the larger overlap.
+#
+# 3) Due to the small size of tRNAs, background correction will struggle to correctly 
+# account for changes in tRNA expression. 
+
+#Loading in necessary packages.
 library(tidyr)
 library(tidyverse)
 
+#Adjustable parameter that will assist in estimation of how much an MDP signal will
+# impact mitogene reads. Particularly important if attempting to background correct
+# tRNAs.
 read_length <- 150
 
+#Function to create an "encompass table" mapping MDPs to mitogenes and calculating
+# percentage of overlap.
 generateEncompassTable <- function(gtf_mdp, gtf_mitochondrial) {
+  #Pre-processing MDP gtf. Removes unnecessary information.
   mdp_gtf <- gtf_mdp
-  mitogene_gtf <- gtf_mitochondrial
-  #Pre-processing MDP gtf.
   colnames(mdp_gtf) <-
-    c("chr",
-      "idc",
-      "idc2",
-      "start",
-      "end",
-      "idc3",
-      "sense",
-      "idc4",
-      "gene")
+    c("chr","idc","idc2", "start","end",
+      "idc3","sense","idc4","gene")
   mdp_gtf <- separate(mdp_gtf, gene, c("gene", "transcript"), ";")
   mdp_gtf <- separate(mdp_gtf, gene, c("trash", "mdp_id"), ">Peptide")
   mdp_db <- mdp_gtf[, c("start", "end", "sense", "mdp_id")]
   mdp_db <- mdp_db[-c(62, 425), ]
   
-  #Pre-processing mitochondrial GTF.
+  #Pre-processing mitochondrial GTF. Removes unnecessary information.
+  mitogene_gtf <- gtf_mitochondrial
   mitogene_db <- mitogene_gtf[, c("V3", "V4", "V5", "V7", "V9")]
   mitogene_db <- mitogene_db[mitogene_db$V3 == "exon", ]
   colnames(mitogene_db) <-
     c("exon", "start", "end", "sense", "mitogene_name")
   mitogene_db$mitogene_name <-
     c(
-      "TF",
-      "RNR1",
-      "TV",
-      "RNR2",
-      "TL1",
-      "ND1",
-      "TI",
-      "TQ",
-      "TM",
-      "ND2",
-      "TW",
-      "TA",
-      "TN",
-      "TC",
-      "TY",
-      "CO1",
-      "TS",
-      "TD",
-      "CO2",
-      "TK",
-      "ATP8",
-      "ATP6",
-      "CO3",
-      "TG",
-      "ND3",
-      "TR",
-      "ND4L",
-      "ND4",
-      "TH",
-      "TS2",
-      "TL2",
-      "ND5",
-      "ND6",
-      "TE",
-      "CYB",
-      "TT",
-      "TP"
+      "TF","RNR1","TV","RNR2","TL1","ND1","TI",
+      "TQ","TM","ND2","TW","TA","TN","TC","TY",
+      "CO1","TS","TD","CO2","TK","ATP8","ATP6",
+      "CO3","TG","ND3","TR","ND4L","ND4","TH",
+      "TS2","TL2","ND5","ND6","TE","CYB","TT","TP"
     )
   
   #Setting up an empty table that will hold overlap # information
@@ -80,36 +65,44 @@ generateEncompassTable <- function(gtf_mdp, gtf_mitochondrial) {
     as.double(encompass_table$proportion)
   
   #Processing which MDPs are in which mitogenes and how much they overlap
-  for (mdp_row in 1:nrow(mdp_db)) {
+  # The way this function works is that it effectively checks for the four overlap possibilities -
+  # the MDP is entirely in the mitogene, the MDP is entirely out of the mitogene, the MDP is
+  # partially inside the mitogene (5' end), or it is partially inside the mitogene (3' end).
+  # The "proportion" parameter will be useful later for calculating the amount of signal that
+  # the MDP is providing to the mitogene reads.
+  for (mdp_row in 1:nrow(mdp_db)) { # Loops through all MDPs.
     mdp_s <- mdp_db[mdp_row, "start"]
     mdp_e <- mdp_db[mdp_row, "end"]
-    mdp_size <- mdp_e-mdp_s
-    for (mito_row in 1:nrow(mitogene_db)) {
+    mdp_size <- mdp_e-mdp_s # MDP size useful for calculating % overlap.
+    for (mito_row in 1:nrow(mitogene_db)) { # Loops through all mitogenes.
       mito_s <- mitogene_db[mito_row, "start"]
       mito_e <- mitogene_db[mito_row, "end"]
-      mito_size <- mito_e-mito_s
-      if ((mdp_s < mito_s &
+      mito_size <- mito_e-mito_s # Mitogene size useful for calculating % overlap.
+      if ((mdp_s < mito_s & # No overlap.
            mdp_s < mito_e & mdp_e < mito_s & mdp_e < mito_e) |
           (mdp_s > mito_s &
            mdp_s > mito_e & mdp_e > mito_s & mdp_e > mito_e)) {
         next;
       }
-      #overlaps early
+      #Checks for partial overlap on 5' end.
       if (mdp_s < mito_s &
           mdp_s < mito_e & mdp_e > mito_s & mdp_e < mito_e) {
         overlap <- (mdp_e - mito_s) / (mdp_e - mdp_s)
         encompass_table <- encompass_table %>% add_row(mdp = mdp_db[mdp_row, "mdp_id"],
                                     mitogene = mitogene_db[mito_row, "mitogene_name"],
-                                    perc_overlap = overlap, proportion=overlap*(((mdp_size)+read_length)/mito_size))
+                                    perc_overlap = overlap, 
+                                    proportion=overlap*(((mdp_size)+read_length)/mito_size))
       } 
-      #overlaps tail
+      #Checks for partial overlap on 3' end.
       if (mdp_s > mito_s &
           mdp_s < mito_e & mdp_e > mito_s & mdp_e > mito_e) {
         overlap <- (mito_e - mdp_s) / (mdp_e - mdp_s)
         encompass_table <- encompass_table %>% add_row(mdp = mdp_db[mdp_row, "mdp_id"],
                                     mitogene = mitogene_db[mito_row, "mitogene_name"],
-                                    perc_overlap = overlap,proportion=overlap*(((mdp_size)+read_length)/mito_size))
+                                    perc_overlap = overlap,
+                                    proportion=overlap*(((mdp_size)+read_length)/mito_size))
       } 
+      #Checks for 100% overlap.
       if (mdp_s > mito_s &
           mdp_s < mito_e & mdp_e > mito_s & mdp_e < mito_e) {
         overlap <- 1
@@ -121,6 +114,10 @@ generateEncompassTable <- function(gtf_mdp, gtf_mitochondrial) {
       } 
     }
   }
+  
+  #A somewhat hacky solution to deal with the case where an MDP overlaps multiple mitogenes.
+  # This code effectively creates a new "encompass table" that only takes in the mitogene with
+  # the highest amount of overlap.
   encompass_table<-encompass_table[!is.na(encompass_table$mdp),]
   encompass_table_new <- data.frame(matrix(ncol = 4, nrow = 0))
   colnames(encompass_table_new) <- c("mdp", "mitogene", "perc_overlap","proportion")
@@ -140,13 +137,17 @@ generateEncompassTable <- function(gtf_mdp, gtf_mitochondrial) {
     }
   }
   encompass_table_new <- drop_na(encompass_table_new)
+  
   #Spitting out the data.
   return (encompass_table_new)
 }
 
+#A fairly straightforward function that determines the amount of background signal in each
+# mitogene by determining deviation from the median. This will lead to a small transformation of
+# the dataset downstream, particularly if there are massive outliers.
 determineBackgroundSignal <- function(mito_counts) {
-  average <- rowMeans(mito_counts[1:ncol(mito_counts)], na.rm=TRUE)
-  mito_counts$average <- average
+  median <- apply(mito_counts, 1, median, na.rm=T)
+  mito_counts$median <- median
   for (mito_column in 1:(ncol(mito_counts)-1)) {
       mito_counts[,mito_column] <- (mito_counts[,mito_column] - mito_counts[,ncol(mito_counts)]) /
         ((mito_counts[,ncol(mito_counts)]))
@@ -155,27 +156,34 @@ determineBackgroundSignal <- function(mito_counts) {
   return (mito_counts)
 }
 
+#Performs background correction of the dataset by subtracting signal generated by
+#mitogenes from the signals of their corresponding MDPs.
 performBackgroundCorrection <- function(background_table,mdp_counts,encompass_table) {
   mdp_names <- rownames(mdp_counts)
-  for (mdp_row in 1:nrow(mdp_counts)) {
-    average_mdp <- rowMeans(mdp_counts[mdp_row,])
+  for (mdp_row in 1:nrow(mdp_counts)) { #Loops through MDPs.
+    #Calculates proportion information for the corresponding mitogene of each MDP.
+    median_mdp <- apply(mdp_counts[mdp_row,], 1, median, na.rm=T)
     mdp <- mdp_names[mdp_row]
     corres_mitogene <- encompass_table$mitogene[encompass_table$mdp==mdp]
     corres_overlap <- encompass_table$perc_overlap[encompass_table$mdp==mdp]
     corres_proportion <- 1-(encompass_table$proportion[encompass_table$mdp==mdp])
-    if (length(corres_proportion) != 0) {
-      if (corres_proportion < 0) {
+    if(length(corres_proportion) != 0) {
+      #If MDP signal on the mitogene is overwhelming, no background
+      # correction is performed.
+      if (corres_proportion < 0) { 
         corres_proportion <- 0
-      } 
-    }
-    if(length(corres_mitogene) != 0) {
-      for (mdp_col in 1:length(mdp_counts[mdp_row,])) {
-        mdp_count <- mdp_counts[mdp_row,mdp_col]
-        background_factor <- background_table[corres_mitogene,mdp_col]
-        corrected_mdp_count <- mdp_count - (background_factor*(corres_proportion)) * average_mdp
-        mdp_counts[mdp_row,mdp_col] <- corrected_mdp_count
-        if(mdp_counts[mdp_row,mdp_col] < 0) {
-          mdp_counts[mdp_row,mdp_col] <- 0
+      } else {
+        for (mdp_col in 1:length(mdp_counts[mdp_row,])) {
+          mdp_count <- mdp_counts[mdp_row,mdp_col]
+          background_factor <- background_table[corres_mitogene,mdp_col]
+          if (!is.nan(background_factor)) {
+            # Subtracts out mitogene signal from MDP signal.
+            corrected_mdp_count <- mdp_count - (background_factor*(corres_proportion)) * median_mdp
+            mdp_counts[mdp_row,mdp_col] <- corrected_mdp_count
+            if(mdp_counts[mdp_row,mdp_col] < 0) {
+              mdp_counts[mdp_row,mdp_col] <- 0
+            }
+          }
         }
       }
     }
--- Cohen/sun2022_analysis.R
+++ Cohen/sun2022_analysis.R
@@ -1,6 +1,12 @@
+#Example corrected MDP-Seq analysis of a dataset investigating leukemia.
+
+#OPTIONAL: List of mitochondrial genes to correct for.
+# Use this to exclude tRNA genes if desired.
+
 #keep_mitogenes <- c("RNR1","RNR2", "ND1","ND2","CO1","CO2","ATP8",
 #                    "ATP6","CO3","ND3","ND4L","ND4","ND5","ND6","CYB")
 
+#Loading in libraries.
 library("BiocManager")
 library("S4Vectors")
 library("Biostrings")
@@ -18,20 +24,26 @@ library(Rsubread)
 library(ggrepel)
 library(circlize)
 
-#Loading in auxiliary code.
+#Loading in auxiliary code. The primary code implementing the algorithm for MDP-Seq
+# lives here.
 setwd("/home/atom/Desktop/AgingProjects/Cohen/")
 source("mdpseq_background_correction.R")
 
+#Loading in auxiliary code. This has a number of "helper functions" that are useful
+# across many pipelines & analyses.
 setwd("/home/atom/Desktop/AgingProjects/Useful Scripts/")
 source("generally_useful.R")
 
+#Relocating to directory where .fastq files and metadata live.
 setwd("/home/atom/Desktop/Data/sun2022")
-#bam_list <- makeBAMS(".",FALSE)
-#mitogene_counts <- getCountsMitochondrial(bam_list,FALSE)
-#mdp_counts <- getCountsMDP(bam_list,FALSE)
+#bam_list <- makeBAMS(".",FALSE) #Converts fastq to BAM.
+#mitogene_counts <- getCountsMitochondrial(bam_list,FALSE) #Creates mitochondrial gene count matrix.
+#mdp_counts <- getCountsMDP(bam_list,FALSE) #Creates MDP count matrix.
 #write.csv(mitogene_counts,"mitogene_counts.csv")
 #write.csv(mdp_counts,"mdp_counts.csv")
 
+#Loads in count matrices and metadata. Appropriately converts them to factors and aligns
+# column names.
 mdp_counts <- read.csv("mdp_counts.csv", row.names=1)
 mitogene_counts <- read.csv("mitogene_counts.csv",row.names=1)
 sun_samplesheet <- read.csv("sun_samplesheet.csv")
@@ -39,14 +51,18 @@ colnames(mdp_counts) <- sun_samplesheet$sample
 colnames(mitogene_counts) <- sun_samplesheet$sample
 sun_samplesheet$diagnosis <- as.factor(sun_samplesheet$diagnosis)
 
+#Performs a standard differential expression analysis for the mitochondrial gene.
+# Not necessary, but can give a rough sense of changes in mitochondrial gene expression.
+# Helps for sanity-checking background correction.
 mitogene_dds <- DESeqDataSetFromMatrix(mitogene_counts,
                                        colData = sun_samplesheet,
                                        design = ~ diagnosis)
 mitogene_dds <- DESeq(mitogene_dds)
-mitogene_res <-data.frame(results(mitogene_dds))
+mitogene_res <- data.frame(results(mitogene_dds))
 mitogene_vsd <- varianceStabilizingTransformation(mitogene_dds) ###mdp-seq script
 mitogene_resOrdered <- mitogene_res[order(abs(mitogene_res$padj)),]
 
+#Creates a volcano plot for mitochondrial gene expression changes.
 mitogene_res$color=factor(case_when(mitogene_res$padj < .05 & abs(mitogene_res$log2FoldChange) >= .60 ~ "purple",
                                     (mitogene_res$padj < .05 & abs(mitogene_res$log2FoldChange) < .60) ~ "red",
                                     (mitogene_res$padj >= .05 & abs(mitogene_res$log2FoldChange) >= .60) ~ "blue",
@@ -62,22 +78,40 @@ ggplot(data=mitogene_res, aes(x=log2FoldChange, y=-log10(padj), color=color, lab
   scale_colour_identity() +
   geom_vline(xintercept = .6,linetype="dotted") +
   geom_vline(xintercept = -.6,linetype="dotted") +
-  labs(title="MitoGene Volcano Plot for IFNa-treated Human T Cells")
+  labs(title="MitoGene Volcano Plot for Leukemia")
 
+#Reads in GTF files. These are being pulled from my local machine, but for convenience I have
+# added them to the github drive as well, in "Useful Scripts."
 mitogene_gtf <-
   read.delim(
     "/home/atom/Desktop/Data/mitochondria_db.gtf",
     header = FALSE,
     comment.char = "#"
-  )
+  ) #Note: This mitochondrial gtf contains only mitochondrial genes.
 mdp_gtf <-
   read.delim("/home/atom/Desktop/Data/mdp_atg_noATC.gtf", header = FALSE)
+# A GTF of MDPs. Obtained from Brendan. 
+# Likely best to remove the mitochondrial gene GTFs from this.
 
+#This next segment of code performs the background correction.
+# generateEncompassTable() is not dependent on the data set. It is responsible for creating a 
+# table that maps out which MDPs are located on which mitochondrial gene (hereafter, "mitogene")
+# and by roughly what proportion.
 encompass_table <- generateEncompassTable(mdp_gtf,mitogene_gtf)
+
+#OPTIONAL: Remove some of the mitochondrial genes. Can be used for excluding tRNAs.
 #encompass_table <- encompass_table[encompass_table$mitogene %in% keep_mitogenes,]
+
+#Creates a table using the mitogene count matrix that maps out relative changes in mitogene
+# expression across samples relative to the mean.
 background_table <- determineBackgroundSignal(mitogene_counts)
+
+#Performs background correction by removing the background signal (calculated above) from
+# the MDP counts relative to their overlap in the mitochondrial gene (calculate in 
+# generateEncompassTable()).
 corrected_counts <- data.frame(performBackgroundCorrection(background_table,mdp_counts,encompass_table))
 
+#Performs a differential gene expression analysis using the corrected counts.
 mdp_dds_corrected <- DESeqDataSetFromMatrix(corrected_counts,
                                             colData = sun_samplesheet,
                                             design = ~ diagnosis)
@@ -87,6 +121,8 @@ mdp_res_corrected <- mdp_res_corrected[!is.na(mdp_res_corrected$padj),]
 mdp_vsd_corrected <- varianceStabilizingTransformation(mdp_dds_corrected) ###mdp-seq script
 mdp_resOrdered_corrected <- mdp_res_corrected[order(abs(mdp_res_corrected$padj)),]
 mdp_top_corrected <- head(mdp_resOrdered_corrected, 10)
+
+#Creates a volcano plot using the corrected counts.
 mdp_res_corrected$color=factor(case_when(mdp_res_corrected$padj < .05 & abs(mdp_res_corrected$log2FoldChange) >= .60 ~ "purple",
                                          (mdp_res_corrected$padj < .05 & abs(mdp_res_corrected$log2FoldChange) < .60) ~ "red",
                                          (mdp_res_corrected$padj >= .05 & abs(mdp_res_corrected$log2FoldChange) >= .60) ~ "blue",
@@ -104,6 +140,7 @@ ggplot(data=mdp_res_corrected, aes(x=log2FoldChange, y=-log10(padj), color=color
   scale_colour_identity() +
   labs(title="MDPSeq Volcano Plot for Leukemia")
 
+#This code performs an uncorrected MDPSeq.
 mdp_dds_uncorrected <- DESeqDataSetFromMatrix(mdp_counts,
                                               colData = sun_samplesheet,
                                               design = ~ diagnosis)
@@ -130,8 +167,11 @@ ggplot(data=mdp_res_uncorrected, aes(x=log2FoldChange, y=-log10(padj), color=col
   scale_colour_identity() +
   labs(title="MDPSeq Volcano Plot for Leukemia - Uncorrected")
 
-#let's try this pizza plot thing - partially Brendan's code.
-#Pre-processing MDP gtf.
+#Creates a pizza plot. Some of the code borrowed here from Brendan & then altered to
+# fix a bug or two.
+
+#Pre-processing MDP gtf. This code removes most of the columns from a GTF and extracts
+# the start, end, sense, and MDP ID.
 colnames(mdp_gtf) <-
   c("chr",
     "idc",
@@ -147,10 +187,14 @@ mdp_gtf <- separate(mdp_gtf, gene, c("trash", "mdp_id"), ">Peptide")
 mdp_db <- mdp_gtf[, c("start", "end", "sense", "mdp_id")]
 mdp_db <- mdp_db[-c(62, 425), ]
 
-pval <- 0.5
+#Aesthetic and significance parameters.
+pval <- 0.05
 h <- 0.05
 borderCol = "gray"
 
+#Extracts top hits in the corrected MDPSeq.
+# Proceeds to map the hits in the corrected MDPSeq and adds positional information from
+# the GTF file processed above.
 top_results <- mdp_res_corrected[mdp_res_corrected$padj < .05 & abs(mdp_res_corrected$log2FoldChange)>.1,]
 top_results$mdp_id <- rownames(top_results) 
 list_of_genes <- data.frame(mdp_id = top_results$mdp_id[order(top_results$padj)], 
@@ -159,6 +203,8 @@ list_of_genes <- data.frame(mdp_id = top_results$mdp_id[order(top_results$padj)]
 positions <- match(list_of_genes$mdp_id,mdp_db$mdp_id)
 positional_table <- merge(drop_na(mdp_db[positions,]),list_of_genes)
 
+#Next ~50 lines of code are to separately process MDPs that are positively or negatively
+# regulated and assign colors to them for pizza plotting.
 positives_fold <- list_of_genes$fold[list_of_genes$fold > 0] %>%
   round(2)
 positives_name <- list_of_genes$mdp_id[list_of_genes$fold > 0]
@@ -182,6 +228,7 @@ positional_table$color <- NULL
 positional_table$color[positives_position] <- positives$list_of_colors_pos
 positional_table$color[negatives_position] <- negatives$list_of_colors_neg
 
+#Begins to draw the pizza plot. This code is primarily Brendan's.
 colors_pos <- data.frame(positives_fold, list_of_colors_pos)
 colors_neg <- data.frame(negatives_fold, list_of_colors_neg)
 plot(c(-1, 1), c(-1, 1), type = "n", axes = FALSE, ann = FALSE, asp = 1)
@@ -227,8 +274,8 @@ legend(x = 1.2, y = 0.5,
        y.intersp = 0.5,
        cex = 1.5, text.font = 2)
 
-#mess w/ mass spec data
+#Looking at mass spec hits here. Pulling data from the mass spec database (provided by
+# Brendan) and seeing if any of the top corrected MDPSeq hits are a match.
 ms_database <- read.csv("~/Desktop/Data/ms_database.csv", comment.char="#")
 ms_database$mdp_id <- gsub(">Peptide", "", ms_database$peptide)
 joined_database <- merge(ms_database,top_results)
-top_hits_sun <- top_results
