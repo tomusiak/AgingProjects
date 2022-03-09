2994b4cb62e416d58c0b6f8c87dd88f350a411b6
tomusiak <atomusiak6@gmail.com>
2022-03-08 20:57:02 -0800

    more Cpg work on mdps

--- Cohen/dmrs_numts.R
+++ Cohen/dmrs_numts.R
@@ -32,10 +32,15 @@ cpg_annotation <- cpg_annotation[,1:5]
 desired_columns <- c(1,2,13,14,15,17)
 numtome <- numtome[,desired_columns]
 colnames(numtome) <- c("MDP","NUMT","chr","start","stop","strand")
+numtome <- numtome[!duplicated(numtome$NUMT),]
+numtome <- numtome[!duplicated(numtome$start),]
+numtome <- numtome[!duplicated(numtome$stop),]
 numtome <- numtome[,c("chr","MDP","start","stop","strand","NUMT")]
 colnames(cpg_annotation) <- c("chr","start","stop","strand","probe_name")
 cpg_table <- data.frame(matrix(ncol = 2, nrow = 0))
 colnames(cpg_table) <- c("cpg","gene")
+upstream <- 500
+downstream <- 2000
 for (cpg in 1:nrow(cpg_annotation)) {
   cpg_chr <- cpg_annotation[cpg,1]
   cpg_start <- cpg_annotation[cpg,2]
@@ -111,7 +116,7 @@ ggplot(data=numt_changes, aes(x=logFC, y=-log10(adj.P.Val), color=color,label=de
   geom_vline(xintercept = -.1,linetype="dotted") +
   geom_text(y=2) +
   scale_colour_identity() +
-  labs(title="Volcano Plot for NUMTs Associated with MDPs w/ Immune Aging")
+  labs(title="Volcano Plot for NUMTs Associated with MDPs w/ Aging")
 
 fit_diff <- lmFit(beta_values,design)
 fit_diff <- eBayes(fit_diff, robust=TRUE)
@@ -124,19 +129,80 @@ numt_changes_diff$NUMT <- combined_mapping_table$NUMT[match(rownames(numt_change
                                                           combined_mapping_table$cpg)]
 numt_changes_diff$MDP <- sub('.Peptide', '',numt_changes_diff$MDP)
 numt_changes_diff$NUMT <- sub('numt_smorf_', '',numt_changes_diff$NUMT)
-numt_changes_diff$color=factor(case_when(numt_changes_diff$adj.P.Val < .05 & abs(numt_changes_diff$logFC) >= .10 ~ "purple",
-                                    (numt_changes_diff$adj.P.Val < .05 & abs(numt_changes_diff$logFC) < .10) ~ "red",
-                                    (numt_changes_diff$adj.P.Val >= .05 & abs(numt_changes_diff$logFC) >= .10) ~ "blue",
-                                    (numt_changes_diff$adj.P.Val >= .05 & abs(numt_changes_diff$logFC) < .10) ~ "gray"))
+numt_changes_diff$color=factor(case_when(numt_changes_diff$adj.P.Val < .05 & abs(numt_changes_diff$logFC) >= .20 ~ "purple",
+                                    (numt_changes_diff$adj.P.Val <= .05 & abs(numt_changes_diff$logFC) <= .20) ~ "red",
+                                    (numt_changes_diff$adj.P.Val >= .05 & abs(numt_changes_diff$logFC) >= .20) ~ "blue",
+                                    (numt_changes_diff$adj.P.Val >= .05 & abs(numt_changes_diff$logFC) < .20) ~ "gray"))
 numt_changes_diff$delabel <- NA
-numt_changes_diff$delabel[numt_changes_diff$color=="purple"] <- numt_changes_diff$NUMT[numt_changes_diff$color=="purple"]
+numt_changes_diff$delabel[numt_changes_diff$color=="purple"] <- numt_changes_diff$MDP[numt_changes_diff$color=="purple"]
 ggplot(data=numt_changes_diff, aes(x=logFC, y=-log10(adj.P.Val), color=color,label=delabel)) + 
   geom_point() +
-  xlim(-.50,.50) + ylim(0,20) +
-  theme_classic(base_size=13)  +
-  geom_hline(yintercept = 1.2,linetype="dotted") +
+  xlim(-.50,.50) + ylim(0,8) +
+  theme_classic(base_size=11)  +
+  geom_hline(yintercept = 1.28,linetype="dotted") +
   geom_vline(xintercept = .1,linetype="dotted") +
   geom_vline(xintercept = -.1,linetype="dotted") +
-  geom_text(size=3.7,position=position_nudge(x=0,y=.5)) +
+  geom_text(size=3.7,position=position_nudge(x=-.04)) +
   scale_colour_identity() +
-  labs(title="Volcano Plot for NUMTs Associated with MDPs w/ Immune Differentiation")
+  labs(title="Volcano Plot for NUMT CpGs Associated with MDPs w/ Immune Differentiation")
+
+grabCPGs <- function(NUMT) {
+  return(cpg_table[cpg_table$NUMT == NUMT,2])
+}
+
+all_NUMTs <- numtome$NUMT
+NUMT <- all_NUMTs[1]
+NUMT <- beta_values[grabCPGs(NUMT),]
+NUMT <- data.frame(t(NUMT))
+NUMT$mean <- rowMeans(NUMT)
+complete_table <- data.frame(t(NUMT$mean))
+colnames(complete_table) <- rownames(NUMT)
+rownames(complete_table) <- all_NUMTs[1]
+
+for (x in 2:length(all_NUMTs)) {
+  NUMT <- all_NUMTs[x]
+  NUMT <- beta_values[grabCPGs(NUMT),]
+  NUMT <- data.frame(t(NUMT))
+  NUMT$mean <- rowMeans(NUMT)
+  NUMT_table <- data.frame(t(NUMT$mean))
+  colnames(NUMT_table) <- rownames(NUMT)
+  rownames(NUMT_table) <- all_NUMTs[x]
+  complete_table <- rbind(complete_table,NUMT_table)
+}
+
+complete_table <- na.omit(complete_table)
+complete_table$MDP <- combined_mapping_table$MDP[match(rownames(complete_table),
+                                                     numtome$NUMT)]
+complete_table <- na.omit(complete_table)
+MDPs <- complete_table %>% group_by(MDP) %>% 
+  summarise(across(everything(), mean))
+MDPs <- data.frame(MDPs)
+rownames(MDPs) <- MDPs[,1]
+MDPs <- data.frame(MDPs)[,-1]
+
+fit_aging <- lmFit(MDPs,age_design)
+fit_aging <- eBayes(fit_aging, robust=TRUE)
+diff_exp_aging <-topTable(fit_aging,coef=5,number=1000000)
+numt_changes <- diff_exp_aging[rownames(diff_exp_aging) %in% cpg_table$cpg,]
+
+fit_diff <- lmFit(MDPs,design)
+fit_diff <- eBayes(fit_diff, robust=TRUE)
+mdp_exp_diff <-topTable(fit_diff,coef=10,number=1000000)
+mdp_exp_diff <- mdp_exp_diff[-7,]
+mdp_exp_diff$color=factor(case_when(mdp_exp_diff$adj.P.Val < .05 & abs(mdp_exp_diff$logFC) >= .20 ~ "purple",
+                                         (mdp_exp_diff$adj.P.Val <= .05 & abs(mdp_exp_diff$logFC) <= .20) ~ "red",
+                                         (mdp_exp_diff$adj.P.Val >= .05 & abs(mdp_exp_diff$logFC) >= .20) ~ "blue",
+                                         (mdp_exp_diff$adj.P.Val >= .05 & abs(mdp_exp_diff$logFC) < .20) ~ "gray"))
+mdp_exp_diff$delabel <- NA
+rownames(mdp_exp_diff) <- sub('.Peptide', '',rownames(mdp_exp_diff) )
+mdp_exp_diff$delabel[mdp_exp_diff$color=="purple"] <- rownames(mdp_exp_diff)[mdp_exp_diff$color=="purple"]
+ggplot(data=mdp_exp_diff, aes(x=logFC, y=-log10(adj.P.Val), color=color,label=delabel)) + 
+  geom_point() +
+  xlim(-.50,.50) + ylim(0,8) +
+  theme_classic(base_size=11)  +
+  geom_hline(yintercept = 1.28,linetype="dotted") +
+  geom_vline(xintercept = .20,linetype="dotted") +
+  geom_vline(xintercept = -.20,linetype="dotted") +
+  geom_text(size=3.7,position=position_nudge(x=-.05,y=0.03)) +
+  scale_colour_identity() +
+  labs(title="Volcano Plot for MDPs (all NUMTs averaged) w/ Immune Differentiation")
