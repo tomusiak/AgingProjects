e09487b037fb5262dfcd3f4a6e347da2d8bb0a1a
tomusiak <atomusiak6@gmail.com>
2022-04-14 15:19:32 -0700

    more figures

--- Cohen/cardio_starved_again_flow.R
+++ Cohen/cardio_starved_again_flow.R
@@ -1,4 +1,4 @@
-flowcardiomyocytes <- read.csv("~/Desktop/Data/second_run.csv")
+flowcardiomyocytes <- read.csv("~/Desktop/Data/signal.csv")
 setwd("/home/atom/Desktop/AgingProjects/Useful Scripts/")
 source("generally_useful.R")
 library(ggplot2)
@@ -34,3 +34,20 @@ ggplot(data=starved_roshigh,aes(x=MDP,y=ROS_Percent)) +
   labs(x="Peptide (10uM)",y="ROS+ % Cells",title="MDP Protection Against Doxo-Induced ROS Production") +
   theme_classic() + scale_fill_brewer(palette="Paired")
 
+starved_roshigh_signal <- getSummary(starved,"ROS_Signal","MDP")
+starved_roshigh_signal$MDP <- factor(starved_roshigh_signal$MDP,levels=c("No Doxo","Doxo Only","63D WT","63D SNP",
+                                                           "108C WT","108C SNP","HNG","NOSH"))
+ggplot(data=starved_roshigh_signal,aes(x=MDP,y=ROS_Signal)) + 
+  geom_bar(stat="identity",position="dodge",fill="lightblue",color="black") +
+  geom_errorbar(aes(ymin=ROS_Signal-se, ymax=ROS_Signal+se), width=.2,position=position_dodge(width=.9)) +
+  labs(x="Peptide (10uM)",y="ROS+ % Cells",title="MDP Protection Against Doxo-Induced ROS Production") +
+  theme_classic() + scale_fill_brewer(palette="Paired")
+
+alive_signal <- getSummary(starved,"Calcein_Signal","MDP")
+alive_signal$MDP <- factor(alive_signal$MDP,levels=c("No Doxo","Doxo Only","63D WT","63D SNP",
+                                                                         "108C WT","108C SNP","HNG","NOSH"))
+ggplot(data=alive_signal,aes(x=MDP,y=Calcein_Signal)) + 
+  geom_bar(stat="identity",position="dodge",fill="lightblue",color="black") +
+  geom_errorbar(aes(ymin=Calcein_Signal-se, ymax=Calcein_Signal+se), width=.2,position=position_dodge(width=.9)) +
+  labs(x="Peptide (10uM)",y="Alive Signal",title="MDP Effects on Doxo-Induced Cell Viability (Mean Alive Signal)") +
+  theme_classic() + scale_fill_brewer(palette="Paired")
