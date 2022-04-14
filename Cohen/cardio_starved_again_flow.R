e7c20fdf4b6c3be19cb316e850054796b9a18976
tomusiak <atomusiak6@gmail.com>
2022-04-13 22:31:41 -0700

    some more mdp

---
+++ Cohen/cardio_starved_again_flow.R
@@ -0,0 +1,37 @@
+flowcardiomyocytes <- read.csv("~/Desktop/Data/second_run.csv")
+setwd("/home/atom/Desktop/AgingProjects/Useful Scripts/")
+source("generally_useful.R")
+library(ggplot2)
+library(RColorBrewer)
+library(dplyr)
+
+starved <- flowcardiomyocytes
+
+starved_alive <- getSummary(starved,"Alive_Percent","MDP")
+starved_alive$MDP <- factor(starved_alive$MDP,levels=c("No Doxo","63D WT","63D SNP",
+                                                       "108C WT","108C SNP","HNG","NOSH","Doxo Only"))
+ggplot(data=starved_alive,aes(x=MDP,y=Alive_Percent)) + 
+  geom_bar(stat="identity",position="dodge",fill="pink",color="black") +
+  geom_errorbar(aes(ymin=Alive_Percent-se, ymax=Alive_Percent+se), width=.2,position=position_dodge(width=.9)) +
+  labs(x="Peptide (10uM)",y="% Not Cell Permeable",title="MDP Effects on Doxo-Induced Cell Permeability (Starved)") +
+  theme_classic() + scale_fill_brewer(palette="Paired")
+
+starved$Alive_Cells <- (6000/starved$CB) * starved$Cells
+
+starved_count <- getSummary(starved,"Cells","MDP")
+starved_count$MDP <- factor(starved_count$MDP,levels=c("No Doxo","63D WT","63D SNP",
+                                                       "108C WT","108C SNP","HNG","NOSH","Doxo Only"))
+ggplot(data=starved_count,aes(x=MDP,y=Cells)) + 
+  geom_bar(stat="identity",position="dodge",fill="lightblue",color="black") +
+  geom_errorbar(aes(ymin=Cells-se, ymax=Cells+se), width=.2,position=position_dodge(width=.9)) +
+  labs(x="Peptide (10uM)",y="Alive Cells Per 10uL",title="MDP Protection Against Doxo-Induced Changes in Cell Counts") +
+  theme_classic() + scale_fill_brewer(palette="Paired")
+
+starved_roshigh <- getSummary(starved,"ROS_Percent","MDP")
+starved_roshigh$MDP <- factor(starved_roshigh$MDP,levels=c("No Doxo","63D WT","63D SNP",
+                                                           "108C WT","108C SNP","HNG","NOSH","Doxo Only"))
+ggplot(data=starved_roshigh,aes(x=MDP,y=ROS_Percent)) + 
+  geom_bar(stat="identity",position="dodge",fill="lightblue",color="black") +
+  geom_errorbar(aes(ymin=ROS_Percent-se, ymax=ROS_Percent+se), width=.2,position=position_dodge(width=.9)) +
+  labs(x="Peptide (10uM)",y="Alive Cells Per 10uL",title="MDP Protection Against Doxo-Induced Changes in Cell Counts") +
+  theme_classic() + scale_fill_brewer(palette="Paired")
