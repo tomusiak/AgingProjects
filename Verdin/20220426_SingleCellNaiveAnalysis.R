fe575514bcd3479f88541a6b98a99185e45d36e5
Alan Tomusiak <atomusiak6@gmail.com>
2022-04-26 22:32:21 -0700

    now?

--- Verdin/20220426_SingleCellNaiveAnalysis.R
+++
@@ -1,5 +0,0 @@
-#Code to analyze some previously-acquired PBMC single-cell data.
-#Goal is to focus on naive T cells and determine if 'naive T cell subsets' exist.
-#If they exist, what markers do they uniquely express?
-
-library(Seurat)
