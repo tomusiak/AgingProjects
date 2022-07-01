0daea8a9290f72e6d209940381d94ff1e85c0677
Alan Tomusiak <atomusiak6@gmail.com>
2022-07-01 01:59:18 +0000

    Fixed a mistake in the validation table; skeleton of clock construction nearly done.

--- Current Projects/20220622_NewClock.R
+++ Current Projects/20220622_NewClock.R
@@ -17,9 +17,42 @@ train_control <- trainControl(method = "repeatedcv",
                               search = "random",
                               verboseIter = TRUE)
 cpg_table_rotated$Age <- sample_table$Age
+small_test <- cpg_table_rotated[,220000:227606]
 training <-train(Age ~ .,
-      data = cpg_table_rotated,
+      data = small_test,
       method = "glmnet",
       preProcess = c("center", "scale"),
       tuneLength = 25,
       trControl = train_control)
+fitted <- predict(training)
+sample_table_2 <- cbind(sample_table,fitted)
+p <- ggplot(sample_table_2,aes(x=Age,y=fitted)) +
+  geom_point() + theme_classic() + geom_smooth(method="lm")
+
+lm_eqn <- function(df){
+  m <- lm(Age ~ fitted, df);
+  eq <- substitute(italic(Age) == a + b %.% italic(fitted)*","~~italic(r)^2~"="~r2, 
+                   list(a = format(unname(coef(m)[1]), digits = 2),
+                        b = format(unname(coef(m)[2]), digits = 2),
+                        r2 = format(summary(m)$r.squared, digits = 3)))
+  as.character(as.expression(eq));
+}
+
+p1 <- p + geom_text(x = 25, y = 300, label = lm_eqn(sample_table_2), parse = TRUE)
+
+ggplotRegression <- function(fit){
+  
+  require(ggplot2)
+  
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point() +
+    stat_smooth(method = "lm", col = "red") +
+    theme_classic() +
+    labs(title = paste("Adj R2 = ",signif(summary(fit)$adj.r.squared, 5),
+                       "Intercept =",signif(fit$coef[[1]],5 ),
+                       " Slope =",signif(fit$coef[[2]], 5),
+                       " P =",signif(summary(fit)$coef[2,4], 5)))
+}
+
+ggplotRegression(lm(Age ~ fitted, sample_table_2))
+
